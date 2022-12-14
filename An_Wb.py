7470004576c97872124f9d13f72d2fe590a47534
ebrahimkhaje <114572627+ebrahimkhaje@users.noreply.github.com>
2022-12-14 06:22:20 +0330

    Add files via upload

---
+++ An_Wb.py
@@ -0,0 +1,64 @@
+class SetClines:
+	PWA = {
+	    "app_name":"Main",
+	    "app_version":"1.2.1",
+	    "platform":"PWA",
+	    "package":"m.rubika.ir"
+	}
+	pwa = {"app_name":"Main","app_version":"1.2.1","package":"m.rubika.ir","platform":"PWA"}
+	
+	web = {
+		"app_name"    : "Main",
+		"app_version" : "4.0.7",
+		"platform"    : "Web",
+		"package"     : "web.rubika.ir",
+		"lang_code"   : "fa"
+	}
+
+	android = {
+	    "app_name":"Main",
+		"app_version":"2.9.5",
+		"platform":"Android",
+		"package":"ir.resaneh1.iptv",
+		"lang_code":"fa"
+	}
+
+
+class Server:
+	matnadress = [
+			'https://messengerg2c26.iranlms.ir',
+			"https://messengerg2c33.iranlms.ir",
+			'https://messengerg2c22.iranlms.ir',
+			'https://messengerg2c25.iranlms.ir',
+			'https://messengerg2c55.iranlms.ir',
+			'https://messengerg2c57.iranlms.ir',
+			'https://messengerg2c15.iranlms.ir',
+			'https://messengerg2c16.iranlms.ir',
+			'https://messengerg2c46.iranlms.ir' ,
+			'https://messengerg2c39.iranlms.ir',
+			'https://messengerg2c40.iranlms.ir',
+			'https://messengerg2c41.iranlms.ir',
+			'https://messengerg2c6.iranlms.ir',
+			'https://messengerg2c7.iranlms.ir',
+			'https://messengerg2c63.iranlms.ir',
+			'https://messengerg2c31.iranlms.ir',
+			'https://messengerg2c3.iranlms.ir',
+			"https://messengerg2c96.iranlms.ir",
+			"https://messengerg2c92.iranlms.ir",
+			"https://messengerg2c32.iranlms.ir",
+			"https://messengerg2c103.iranlms.ir",
+			"https://messengerg2c115.iranlms.ir",
+			"https://messengerg2c46.iranlms.ir"
+			]
+
+
+	filesadress = [
+			'https://messengerg2c26.iranlms.ir'
+			'https://messengerg2c39.iranlms.ir',
+			'https://messengerg2c7.iranlms.ir',
+			'https://messengerg2c17.iranlms.ir',
+			'https://messengerg2c46.iranlms.ir'
+			]
+
+
+
